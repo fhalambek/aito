898cf5d05bcf50a78ad05a9b84c74e1119c6fe09
fhalambek <fran.halambek@gmail.com>
2018-09-01 00:15:36 +0200

    Add files via upload

--- autic_v2/autic_v2.ino
+++ autic_v2/autic_v2.ino
@@ -1,23 +1,32 @@
 #include <stdlib.h>
 
-/*
-const int trigPin[0] = 3;
-const int echoPin[0] = 2;
-const int trigPin[1] = 4;
-const int echoPin[1] = 5;
-*/
-const int foward_l = 10;
-const int foward_r = 9;
-const int back_l = 12;
-const int back_r = 11;
 
+const int trigPinL = 6;
+const int echoPinL = 5;
+const int trigPinR= 12;
+const int echoPinR = 11;
 
+const int foward_l = 9;
+const int foward_r = 10;
+const int back_l = 7;
+const int back_r = 8;
+
+const int redR = 16;
+const int greenR = 17;
+const int redL = 14;
+const int greenL = 15;
 
 void setup() {
   pinMode(back_r, OUTPUT);     
   pinMode(back_l, OUTPUT);     
   pinMode(foward_r, OUTPUT);   
   pinMode(foward_l, OUTPUT);   
+
+  pinMode(redR, OUTPUT);
+  pinMode(greenR, OUTPUT);
+  pinMode(redL, OUTPUT);
+  pinMode(greenL, OUTPUT);
+  
   
   //pinMode(6, INPUT);     //ir
   
@@ -26,9 +35,51 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(10, HIGH);
+  //drive(100);
+
+  lights();
   
-  //digitalWrite(9, HIGH);
+  if(distR() < 30 or distL() < 30){
+    int s = rand();
+    brake();
+    
+    drive(-100);
+    delay(700);
+    lights();
+    if (s % 2 == 0) {
+      leftMotor(100);
+      rightMotor(-100);
+    }
+    else {
+      rightMotor(100);
+      leftMotor(-100);
+    }
+    while(distL() <= 85 or distR() <= 85);
+    lights();
+    delay(rand()%200 + 100);
+    lights();
+    brake();
+  }
+  if(distR() < 60 and distL() < 60){
+    drive(75);
+  }
+  else if(distR() < 60){
+    leftMotor(100);
+    rightMotor(0);
+    delay(400);
+  }
+  else if(distL() < 60){
+    leftMotor(0);
+    rightMotor(100);
+    delay(400);
+  }
+  //else if(distR() < 30
+  
+  drive(100);
+  
+  lights();
+  
+  
   /*
   for(int i = -100; i <= 100; i++){
     drive(i);
@@ -39,15 +90,19 @@ void loop() {
     delay(50);
   }
   */
+
+  
 }
+
+
 
 
 void leftMotor(int percent){
   int power = (percent * 255)/100;
   if(power >= 0){
-  if(digitalRead(back_l == HIGH)){
+    if(digitalRead(back_l == HIGH)){
       digitalWrite(back_l, LOW);
-      delay(150);
+      //delay(150);
     }
     analogWrite(foward_l, power);
   }
@@ -61,7 +116,7 @@ void rightMotor(int percent){
   if(power >= 0){
     if(digitalRead(back_r == HIGH)){
       digitalWrite(back_r, LOW);
-      delay(150);
+      //delay(150);
     }
     analogWrite(foward_r, power);
   }
@@ -69,6 +124,37 @@ void rightMotor(int percent){
     digitalWrite(back_r, HIGH);
     analogWrite(foward_r, 255+power);
   }
+}
+
+void lights(){
+  
+  if(distL() < 30){
+    digitalWrite(greenL, LOW);
+    digitalWrite(redL, HIGH);
+  }
+  else if(distL() < 60){
+    digitalWrite(greenL, HIGH);
+    digitalWrite(redL, HIGH);
+  }
+  else{
+    digitalWrite(greenL, HIGH);
+    digitalWrite(redL, LOW);
+  }
+
+  
+  if(distR() < 30){
+    digitalWrite(greenR, LOW);
+    digitalWrite(redR, HIGH);
+  }
+  else if(distR() < 60){
+    digitalWrite(greenR, HIGH);
+    digitalWrite(redR, HIGH);
+  }
+  else{
+    digitalWrite(greenR, HIGH);
+    digitalWrite(redR, LOW);
+  }
+  
 }
 
 void drive(int power){
@@ -80,24 +166,42 @@ void brake(){
   rightMotor(0);
   delay(150);
 }
-/*
-int dist(int side){
+
+
+
+int distR(){
   int duration, cm;
   
-  pinMode(trigPin[side], OUTPUT);
-  digitalWrite(trigPin[side], LOW);
+  pinMode(trigPinR, OUTPUT);
+  digitalWrite(trigPinR, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin[side], HIGH);
+  digitalWrite(trigPinR, HIGH);
   delayMicroseconds(5);
-  digitalWrite(trigPin[side], LOW);
+  digitalWrite(trigPinR, LOW);
 
-  pinMode(echoPin[side], INPUT);
-  duration = pulseIn(echoPin[side], HIGH, 300*2*29);
+  pinMode(echoPinR, INPUT);
+  duration = pulseIn(echoPinR, HIGH, 300*2*29);
 
   cm = microsecondsToCentimeters(duration);
   return cm;
 }
-*/
+int distL(){
+  int duration, cm;
+  
+  pinMode(trigPinL, OUTPUT);
+  digitalWrite(trigPinL, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinL, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPinL, LOW);
+
+  pinMode(echoPinL, INPUT);
+  duration = pulseIn(echoPinL, HIGH, 300*2*29);
+
+  cm = microsecondsToCentimeters(duration);
+  return cm;
+}
+
 long microsecondsToCentimeters(long microseconds) {
   if(microseconds == 0) return 1000;
   return microseconds / 29 / 2;
